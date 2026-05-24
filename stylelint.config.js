1191129e1094f4531fff888562e2ded3acd201a9
Frankie <1426203851@qq.com>
2026-05-24 13:46:45 +0800

    chore: add update-obsidian-theme script

--- stylelint.config.js
+++ stylelint.config.js
@@ -6,5 +6,6 @@ export default {
     'no-descending-specificity': null,
     'selector-pseudo-element-no-unknown': null,
     'custom-property-pattern': null,
+    'selector-class-pattern': null,
   },
 }
